5c0fbef932ad8da84ba8eb721474d74846f89346
ghobtr <emresonmez@outlook.com>
2018-11-06 23:21:58 +0300

    Create train_chatbot.py

---
+++ train_chatbot.py
@@ -0,0 +1,16 @@
+def train_from_file(fname):
+    bot = ChatBot(
+    "Terminal",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    logic_adapters=[
+        "chatterbot.logic.MathematicalEvaluation",
+        "chatterbot.logic.TimeLogicAdapter",
+        "chatterbot.logic.BestMatch"
+    ],
+    input_adapter="chatterbot.input.TerminalAdapter",
+    output_adapter="chatterbot.output.TerminalAdapter",
+    database="db.sqlite3"
+    )
+    chatterbot.set_trainer(ListTrainer)
+    trainer = ListTrainer(bot)
+    trainer.train(fname)
